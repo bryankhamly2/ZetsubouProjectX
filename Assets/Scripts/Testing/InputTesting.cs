70c11ce7caff34b7d669336a5779591856909d10
Unknown <kohina.ow@gmail.com>
2018-01-10 06:52:24 -0800

    Added: Rewired

---
+++ Assets/Scripts/Testing/InputTesting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class InputTesting : MonoBehaviour
+{
+    public int playerId = 0;
+    private Player player;
+
+    public float Horizontal;
+    public float Vertical;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = player.GetAxisRaw(0);
+        Vertical = player.GetAxisRaw(1);
+    }
+}
